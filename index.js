29c2576f700678a27c0dc09c6e9c4b98a8dcff2c
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-15 23:40:31 +0530

    Add: Index page created

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import connectDB from "./config/db.js";
+import notesRoute from './routes/notes.route.js'
+import { errorHandler, notFound } from "./middlewares/error.middleware.js";
+dotenv.config();
 
-const app = express()
+const PORT = process.env.PORT;
+
+connectDB()
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:5173' }))
+
+app.use('/api/notes', notesRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started listening on ${PORT}.....`))
