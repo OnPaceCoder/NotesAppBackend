bc76551210d1aad81d4181306ffa6f4ebceca62a
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-18 18:43:43 +0530

    fix: database config

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
-import exp from "constants";
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Mongodb connected to host ${conn.connection.host}`)
+        console.log(`Mongodb connected to host successfully`)
 
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit()
     }
 }
 
